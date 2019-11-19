f29784504f0ab430c173a618bc519378d851813b
Oscar <neonode2014@gmail.com>
2019-11-19 19:22:38 +0100

    new GUI layout

--- gui.py
+++ gui.py
@@ -3,83 +3,54 @@ from appJar import gui
 
 app = gui()
 
-
-
 IP = '127.0.0.1'
 PORT = 6663
 SERVER_INFO = 'You successfully connected to: ' + IP + ':' + str(PORT)
 
-connections = ['  ## CONNECTIONS ##', 'racso', 'mercur', 'hyperX', '127.0.0.1']
-
-busy_nums = []
-
-# FUNCTIONS
-def random_name():
-    ran = random.randint(1, 1000)
-    if ran not in busy_nums:
-        busy_nums.append(ran)
-        return ran
-    else:
-        random_name()
+connections = ['racso', 'mercur', 'hyperX', '127.0.0.1', 'oSCar', 'willepille']
 
 
 def update_inputs():
     app.addListItem(title='chatbox', item='Test', select=False)
 
 
-# POSITION GROUPS
-x_0 = 0
-x_1 = 1
-x_2 = 2
-x_3 = 3
-x_4 = 4
-x_5 = 5
-x_6 = 6
-x_7 = 7
-x_8 = 8
-
-y_0 = 0
-y_1 = 1
-y_2 = 2
-y_3 = 3
-y_4 = 4
-y_5 = 5
-y_6 = 6
-y_7 = 7
-y_8 = 8
-y_9 = 9
-y_10 = 10
-
 # BODY SETTINGS
 # app.setSize(x, y)
-app.setSize(600, 400)
+app.setSize(600, 600)
 app.setFont(14)
 app.setBg('LightGrey')
-app.setTransparency(97)
+app.setTransparency(99)
 
 
-for y in range(15):
-    app.addLabel(f'y--x_0_y_{y}', '[]', row=x_0, column=y_0+y)
+# creating 40 columns (y)
+for y in range(40):
+    app.addLabel(f'y--x_0_y_{y}', f'', row=0, column=0+y)
 
-for x in range(15):
-    app.addLabel(f'x--x_{x}_y_0', '{}', row=x_1+x, column=y_0)
+# creating 15 rows (x)
+for x in range(25):
+    app.addLabel(f'x--x_{x}_y_0', f'', row=1+x, column=0)
 
 
+app.addLabel('connected_users', 'CONNECTED', row=4, column=35)
+app.addLabel('chat', 'CHAT', row=4, column=15)
 
+app.addVerticalSeparator(row=2, column=30, rowspan=21, colour="grey")
 
+app.addListBox(name='chatbox',
+               values=[SERVER_INFO],
+               row=6,
+               column=1,
+               rowspan=15,
+               colspan=28)
 
-# app.addVerticalSeparator(row=x_0, column=y_5, rowspan=1, colour="grey")
+app.addListBox(name='connected',
+               values=connections,
+               row=6,
+               column=32,
+               rowspan=15,
+               colspan=7)
 
-# app.addListBox(name='chatbox',
-#                values=[SERVER_INFO],
-#                row=x_0,
-#                column=y_1,)
-
-# app.addListBox(name='connected',
-#                values=connections,
-#                row=x_0,
-#                column=y_7,)
-
+app.addTextArea('message', row=22, column=3, colspan=24, rowspan=1)
 
 app.go()
 
