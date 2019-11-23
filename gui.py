a640d62e86795e986cf6a2123bcf0870ce97b0fb
Oscar <neonode2014@gmail.com>
2019-11-23 15:55:44 +0100

    GUI functions are ready for integration

--- gui.py
+++ gui.py
@@ -10,8 +10,18 @@ SERVER_INFO = 'You successfully connected to: ' + IP + ':' + str(PORT)
 connections = ['racso', 'mercur', 'hyperX', '127.0.0.1', 'oSCar', 'willepille']
 
 
-def update_inputs():
-    app.addListItem(title='chatbox', item='Test', select=False)
+def update_inputs(text):
+    app.addListItem(title='chatbox', item=text, select=False)
+
+
+def click(buttontype):
+    if buttontype == "Send":
+        update_inputs(app.getTextArea('message'))
+        app.clearTextArea(title='message', callFunction=False)
+    elif buttontype == "Clear":
+        app.clearListBox(title='chatbox', callFunction=False)
+    else:
+        pass
 
 
 # BODY SETTINGS
@@ -24,11 +34,11 @@ app.setTransparency(99)
 
 # creating 40 columns (y)
 for y in range(40):
-    app.addLabel(f'y--x_0_y_{y}', f'', row=0, column=0+y)
+    app.addLabel(f'y--x_0_y_{y}', '', row=0, column=0+y)
 
 # creating 15 rows (x)
 for x in range(25):
-    app.addLabel(f'x--x_{x}_y_0', f'', row=1+x, column=0)
+    app.addLabel(f'x--x_{x}_y_0', '', row=1+x, column=0)
 
 
 app.addLabel('connected_users', 'CONNECTED', row=4, column=35)
@@ -51,6 +61,8 @@ app.addListBox(name='connected',
                colspan=7)
 
 app.addTextArea('message', row=22, column=3, colspan=24, rowspan=1)
+
+app.addButtons(['Send', 'Clear'], click)
 
 app.go()
 
